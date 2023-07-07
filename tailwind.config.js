fca3ac23a59be57ac703b7eb5c344813a68dc945
gwowg <guogang73@outlook.com>
2023-07-07 18:54:21 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,7 +16,6 @@ export default {
         }
       },
       fontFamily: {
-        lxgw: ['LXGWBrightGB-Regular', 'sans-serif'],
         sans: FONTS_SANS,
         serif: FONTS_SERIF,
         noEmoji: [
