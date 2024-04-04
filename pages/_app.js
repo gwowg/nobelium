7a3f44ee454ac55f81465944a9c90b906fab97f0
gwowg <guogang73@outlook.com>
2024-04-04 21:29:49 +0800

    重新安装 pnpm install --legacy-peer-deps

--- pages/_app.js
+++ pages/_app.js
@@ -64,3 +64,16 @@ function MyApp({ Component, pageProps }) {
 }
 
 export default MyApp;
+
+import { SpeedInsights } from '@vercel/speed-insights/next';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <SpeedInsights />
+    </>
+  );
+}
+
+export default MyApp;
